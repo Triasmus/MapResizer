1a6d31706ee2268a9db2b4755cfa43b30100afd9
Triasmus <Github@alwilcox.com>
2020-07-17 14:50:16 -0600

    put in the math the scale the image

--- src.js
+++ src.js
@@ -22,13 +22,13 @@ function doFunction(){
 	
 	var approxCellx = coords[1].x - coords[0].x;
 	var canvasX = coords[2].x - coords[0].x;
-	var xResizePercent = 100 * (Math.round(canvasX/approxCellx) * newPixelWidth)/canvasX
+	var xResizeScale = (Math.round(canvasX/approxCellx) * newPixelWidth)/canvasX
 	
 	var approxCelly = coords[1].y - coords[0].y;
 	var canvasY = coords[2].y - coords[0].y;
-	var yResizePercent = 100 * (Math.round(canvasY/approxCelly) * newPixelWidth)/canvasY
+	var yResizeScale = (Math.round(canvasY/approxCelly) * newPixelWidth)/canvasY
 
-    document.getElementById("results2").innerHTML = "X scaling: " + xResizePercent + "    Y scaling: " + yResizePercent;
+    document.getElementById("results2").innerHTML = "X scaling: " + xResizeScale + "    Y scaling: " + yResizeScale;
     
     const elem = document.createElement('canvas');
     elem.width = 0;
@@ -42,7 +42,7 @@ function doFunction(){
         var ctx = c.getContext("2d");
         var img = document.getElementById("blah");
         // TODO DO MATH
-        ctx.drawImage(img, 10, 10, 100, 100);
+        ctx.drawImage(img, 10, 10, imageWidth * xResizeScale, imageHeight * yResizeScale);
         ctx.canvas.toBlob((blob) => {
             download(blob, "test.png", "application/json");
         }, 'image/png', 1);    
